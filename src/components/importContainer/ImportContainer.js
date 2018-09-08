34a5ab7eb106be2bd4ec7ad077f2a199903f9346
Kiel Dehnert <kielzor@gmail.com>
2018-09-07 18:30:09 -0600

    Build ImportAudio component

--- src/components/importContainer/ImportContainer.js
+++ src/components/importContainer/ImportContainer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './ImportContainer.css'
 
 export const ImportContainer = () => {
   return (
-    <div>
-      <h1>Import</h1>
+    <div className='import-container'>
+      <h1>Import Audio</h1>
+      <div className='import-audio'>
+        <input placeholder='Name'  className='import-input'/>
+        <select className='import-input'>
+          <option default>Speed</option>
+          <option>Warmup/ Cooldown</option>
+          <option>Jog</option>
+          <option>Sprint</option>
+        </select>
+        <div>
+          <input type='file' accept='audio/*' placholder='File' className='import-file-input'/>
+          <button className='import-save-button'>Save</button>
+        </div>
+      </div>
     </div>
   )
 }
